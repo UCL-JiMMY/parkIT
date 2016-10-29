80060cf1f041233dcd4c3d4cbc95b5192f3a808e
Matt Policane <mattpolicane@gmail.com>
2016-10-29 22:25:27 +0100

    Second one

--- parkIT/parkIT/ViewController.swift
+++ parkIT/parkIT/ViewController.swift
@@ -9,38 +9,62 @@
 import UIKit
 import GoogleMaps
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITextFieldDelegate {
 
+    //Properties
+    @IBOutlet weak var locationText: UITextField!
+    
     @IBOutlet weak var mapView: GMSMapView!
     
+    @IBOutlet weak var latText: UITextField!
     // You don't need to modify the default init(nibName:bundle:) method.
     
     override func loadView() {
         // Create a GMSCameraPosition that tells the map to display the
         // coordinate -33.86,151.20 at zoom level 6.
-        let camera = GMSCameraPosition.camera(withLatitude: -33.86, longitude: 151.20, zoom: 6.0)
+        let camera = GMSCameraPosition.camera(withLatitude: 51.519, longitude: -0.124, zoom: 12.0)
         let mapView = GMSMapView.map(withFrame: CGRect.zero, camera: camera)
         mapView.isMyLocationEnabled = true
         view = mapView
         
         // Creates a marker in the center of the map.
         let marker = GMSMarker()
-        marker.position = CLLocationCoordinate2D(latitude: -33.86, longitude: 151.20)
-        marker.title = "Sydney"
-        marker.snippet = "Australia"
+        marker.position = CLLocationCoordinate2D(latitude: 51.519, longitude: -0.124)
+        //marker.title = "Sydney"
+        //marker.snippet = "Australia"
         marker.map = mapView
     }
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        //locationText.delegate = self
+        //latText.delegate = self
+
+
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
+    
+    // MARK: UITextFieldDelegate
+   /* func textFieldShouldReturn(textField: UITextField) -> Bool {
+        // Hide the keyboard.
+        textField.resignFirstResponder()
+        return true
+    }*/
+    
+ 
+    
+    //Actions
+    
+    @IBAction func buttonPress(_ sender: Any) {
+        
+        
+    }
+    
 
 }
 
